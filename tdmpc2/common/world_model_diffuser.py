a4be901d7a4a5a281862b8d724942d623a00a54a
MossForest <1596375272@qq.com>
2025-01-30 15:02:47 +0800

    fix: do not clear Diffuser when initial modal

--- tdmpc2/common/world_model_diffuser.py
+++ tdmpc2/common/world_model_diffuser.py
@@ -22,12 +22,12 @@ class WorldModelDiffuser(nn.Module):
         self.cfg = cfg
         self._encoder = layers.enc(cfg)
         # self._dynamics = layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], cfg.latent_dim, act=layers.SimNorm(cfg))
-        self._dynamics = self.load_diffuser(cfg.diffuser_pretrained_path)
         self._reward = layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], max(cfg.num_bins, 1))
         # self._pi = layers.mlp(cfg.latent_dim + cfg.task_dim, 2*[cfg.mlp_dim], 2*cfg.action_dim)
         self._Qs = layers.Ensemble([layers.mlp(cfg.latent_dim + cfg.action_dim + cfg.task_dim, 2*[cfg.mlp_dim], max(cfg.num_bins, 1), dropout=cfg.dropout) for _ in range(cfg.num_q)])
         self.apply(init.weight_init)
         init.zero_([self._reward[-1].weight, self._Qs.params["2", "weight"]])
+        self._dynamics = self.load_diffuser(cfg.diffuser_pretrained_path)
 
         self.register_buffer("log_std_min", torch.tensor(cfg.log_std_min))
         self.register_buffer("log_std_dif", torch.tensor(cfg.log_std_max) - self.log_std_min)
